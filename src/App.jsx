e26890fef6362dbe079939340b547248d0c686ee
GhostMobin <s1810677110@ru.ac.bd>
2023-09-15 12:24:57 +0600

    toast added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import Course from "./components/Course";
 import Cart from "./components/Cart";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [courses, setCourses] = useState([]);
-  const [cart, setCart] = useState([]);
-  // const [credits, setCredits] = useState(0);
+  const [totalCredits, setTotalCredits] = useState(0);
+  const [remainingCredits, setRemainingCredits] = useState(20);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [courseNames, setCourseNames] = useState([]);
 
   useEffect(() => {
     fetch("courses.json")
@@ -15,16 +17,33 @@ function App() {
   }, []);
 
   const handleSelect = (course) => {
-    const newCart = [...cart, course];
-    setCart(newCart);
+    if (courseNames.includes(course.title)) {
+      toast.error("You've already selected this course.");
+      return;
+    }
+
+    const newTotalCredits = totalCredits + course.credit;
+    if (newTotalCredits > 20) {
+      toast.error("Credit hours must not exceed 20 hours.");
+      return;
+    }
+
+    const newRemainingCredits = remainingCredits - course.credit;
+    const newTotalPrice = totalPrice + course.price;
+
+    setTotalCredits(newTotalCredits);
+    setRemainingCredits(newRemainingCredits);
+    setTotalPrice(newTotalPrice);
+    setCourseNames([...courseNames, course.title]);
   };
 
   return (
     <>
+      <Toaster position="top-center" reverseOrder={true} />
       <h1 className="font-bold text-3xl text-center mt-12">
         Course Registration
       </h1>
-      <main className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 my-8 mx-6 xl:mx-16 gap-y-6 md:gap-6">
+      <main className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-8 mb-16 mx-6 xl:mx-16 gap-y-6 md:gap-6">
         <section className="xl:col-span-3 grid grid-cols-1 xl:grid-cols-3 gap-6">
           {courses.map((course) => (
             <Course
@@ -35,7 +54,13 @@ function App() {
           ))}
         </section>
         <aside>
-          <Cart cart={cart} />
+          {/* <Cart cart={cart} /> */}
+          <Cart
+            totalCredits={totalCredits}
+            remainingCredits={remainingCredits}
+            totalPrice={totalPrice}
+            courseNames={courseNames}
+          />
         </aside>
       </main>
     </>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,31 +1,21 @@
 import PropTypes from "prop-types";
-import CartItems from "./CartItems";
 
-const Cart = ({ cart }) => {
-  //   const { credit, price } = cart;
-
-  const totalCreditHour = cart.reduce((accumulator, item) => {
-    return accumulator + item.credit;
-  }, 0);
-  const totalPrice = cart.reduce((accumulator, item) => {
-    return accumulator + item.price;
-  }, 0);
-
+const Cart = ({ totalCredits, remainingCredits, totalPrice, courseNames }) => {
   return (
     <div className="rounded-xl p-6 bg-white h-auto">
       <h2 className="font-bold text-xl text-blue-500">
-        Credit Hour Remaining {20 - totalCreditHour} hr
+        Credit Hour Remaining {remainingCredits} hr
       </h2>
       <hr className="my-4" />
       <h2 className="font-bold text-2xl mb-5">Course Name</h2>
       <ol className="list-decimal list-inside leading-8 text-lg text-slate-600">
-        {cart.map((cartItem) => (
-          <CartItems key={cartItem.id} cartItem={cartItem.title} />
+        {courseNames.map((courseName, index) => (
+          <li key={index}>{courseName}</li>
         ))}
       </ol>
       <hr className="mt-6 mb-4" />
       <h4 className="font-medium text-slate-700 text-lg">
-        Total Credit Hour : {totalCreditHour}
+        Total Credit Hour : {totalCredits}
       </h4>
       <hr className="my-4" />
       <h3 className="font-semibold text-slate-700 text-lg">
@@ -36,7 +26,10 @@ const Cart = ({ cart }) => {
 };
 
 Cart.propTypes = {
-  cart: PropTypes.array,
+  totalCredits: PropTypes.number,
+  remainingCredits: PropTypes.number,
+  totalPrice: PropTypes.number,
+  courseNames: PropTypes.array,
 };
 
 export default Cart;
